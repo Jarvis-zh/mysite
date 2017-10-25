614aac00e6dbf7a1af87c477a28d31fcf8b6625a
Jarvis-zh <jarvis_zh@163.com>
2017-10-25 21:24:43 +0800

    【修改功能】创建为blog数据模型类，先使用默认数据库sqlite，迁移数据。
    数据模型：文章Post, 分类Category, 标签Tag

--- myblog/models.py
+++ myblog/models.py
@@ -1,3 +1,49 @@
+#-*- coding:utf-8 -*-
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+
+class Category(models.Model):
+    """
+    Category : 文章的分类归档，每篇文章只能有一种分类归档
+    """
+    # 分类名称
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Tag(models.Model):
+    """
+    Tag : 文章的标签，每篇文章可能有多个标签
+    """
+    # 标签名称
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    """
+    Post : 文章主体
+    """
+    # 标题
+    title = models.CharField(max_length=100)
+    # 内容
+    body = models.TextField()
+    # 创建时间
+    created_time = models.DateTimeField()
+    # 修改时间
+    modified_time = models.DateTimeField()
+    # 文章摘要 ，可以为空
+    excerpt = models.CharField(max_length=100)
+    # 文章作者
+    author = models.ForeignKey(User)
+    # 文章分类
+    category = models.ForeignKey(Category)
+    # 文章标签, 允许为空
+    tags = models.ManyToManyField(Tag, blank=True)
+
+    def __str__(self):
+        return self.title
