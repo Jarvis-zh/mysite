c3d727d2e935357d2c78283c27b4d8198ac1d7fc
Jarvis-zh <jarvis_zh@163.com>
2017-11-18 11:00:56 +0800

    [修改功能]修改页面视图代码，采用基于通用ListView和DetailView类视图实现。

--- myblog/urls.py
+++ myblog/urls.py
@@ -4,11 +4,11 @@ from . import views
 
 app_name = 'myblog'
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^(?P<post_id>[0-9]+)/$', views.detail, name='detail'),
-    url(r'^archives/(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/$', views.archives, name='archives'),
-    url(r'^category/(?P<pk>[0-9]+)/$', views.category, name='category'),
-    url(r'^tag/(?P<pk>[0-9]+)/$', views.tags, name='tag'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^archives/(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/$', views.ArchivesView.as_view(), name='archives'),
+    url(r'^category/(?P<pk>[0-9]+)/$', views.CategoryView.as_view(), name='category'),
+    url(r'^tag/(?P<pk>[0-9]+)/$', views.TagsView.as_view(), name='tag'),
+    url(r'^(?P<pk>[0-9]+)/$', views.PostDetailView.as_view(), name='detail'),
     url(r'^about/$', views.about, name='about'),
     url(r'^contact/$', views.contact, name='contact'),
 ]
--- myblog/views.py
+++ myblog/views.py
@@ -1,58 +1,47 @@
 #-*- coding:utf-8 -*-
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
 from .models import Post, Category, Tag
 
-def index(request):
-    post_list = Post.objects.all()
-    page_title = '首页'
-    context = {'post_list' : post_list,
-               'page_title' : page_title}
-    return render(request, 'myblog/index.html', context)
 
-def detail(request, post_id):
-    post = get_object_or_404(Post, pk=post_id)
+#首页
+class IndexView(ListView):
+    model = Post
+    template_name = 'myblog/index.html'
+    context_object_name = 'post_list'
 
-    #粗略统计阅读量
-    post.views += 1
+#归档页面
+class ArchivesView(IndexView):
+    def get_queryset(self):
+        year = self.kwargs.get('year')
+        month = self.kwargs.get('month')
+        return super(ArchivesView, self).get_queryset().filter(created_time__year=year,
+                                                               created_time__month=month)
+#分类
+class CategoryView(IndexView):
+    def get_queryset(self):
+        cate = get_object_or_404(Category, pk=self.kwargs.get('pk'))
+        return super(CategoryView, self).get_queryset().filter(category=cate)
+#标签
+class TagsView(IndexView):
+    def get_queryset(self):
+        tag = get_object_or_404(Tag, pk=self.kwargs.get('pk'))
+        return super(TagsView, self).get_queryset().filter(tags=tag)
 
-    context = {'post': post}
-    return render(request, 'myblog/detail.html', context)
+#文章全文
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'myblog/detail.html'
+    context_object_name = 'post'
+
+    def get(self, request, *args, **kwargs):
+        response = super(PostDetailView, self).get(self, request, *args, **kwargs)
+        self.object.increase_views() #每次获取简单计数，认为阅读量加一
+        return response
+
 
 def about(request):
     return render(request, 'myblog/about.html')
 
 def contact(request):
     return render(request, 'myblog/contact.html')
-
-#归档页面
-def archives(request, year, month):
-    post_list = Post.objects.filter(created_time__year=year,
-                                    created_time__month=month).order_by('-created_time')
-    page_title = '{}年{}月文章'.format(year, month)
-    context = {
-        'post_list' : post_list,
-        'page_title' : page_title
-    }
-    return render(request, 'myblog/index.html', context)
-
-#分类页面
-def category(request, pk):
-    temp_category = get_object_or_404(Category, pk=pk)
-    post_list = Post.objects.filter(category=temp_category).order_by('-created_time')
-    page_title = '{}类文章'.format(temp_category)
-    context = {
-        'post_list' : post_list,
-        'page_title' : page_title
-    }
-    return render(request, 'myblog/index.html', context)
-
-#标签云页面
-def tags(request, pk):
-    temp_tag = get_object_or_404(Tag, pk=pk)
-    post_list = Post.objects.filter(tags=temp_tag).order_by('-created_time')
-    page_title = '{}标签文章'.format(temp_tag)
-    context = {
-        'post_list' : post_list,
-        'page_title' : page_title
-    }
-    return render(request, 'myblog/index.html', context)
